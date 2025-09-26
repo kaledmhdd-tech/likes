5413e077e6b5e38dda49dc8c01cc05531587ef6c
sishev6328eh-creator <sishev6328eh@gmail.com>
2025-09-26 15:24:21 +0100

    Update send_like.py

--- api/send_like.py
+++ api/send_like.py
@@ -105,7 +105,7 @@ def send_like():
 
     # جلب معلومات اللاعب قبل الإرسال
     try:
-        info_url = f"https://info-self-one.vercel.app/get?uid={player_id}"
+        info_url = f"https://info-six-theta.vercel.app/get?uid={player_id}"
         resp = httpx.get(info_url, timeout=10)
         info_json = resp.json()
         account_info = info_json.get("AccountInfo", {})
